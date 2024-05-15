5dc5ad96c6017de125c13f524f26bfcf4104f6c6
YenielG <157734065+YenielG@users.noreply.github.com>
2024-05-15 12:27:44 -0400

    Update html_header.php

--- public/html_header.php
+++ public/html_header.php
@@ -1,3 +1,5 @@
+//header del html 
+
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
